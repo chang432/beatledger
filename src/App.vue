9a2fac54031466684592e83692ac7ba61e39fd77
andre <chang432@purdue.edu>
2022-01-27 21:21:07 -0500

    vue setup + downloading needed npm packages

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <BeatList />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BeatList from "./components/BeatList.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { BeatList },
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/BeatList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>HELLO WORLD</h1>
+  </div>
+</template>
+
+<script>
+// import Arweave from "arweave";
+// import ArDB from "ardb";
+
+// test gateway for Arlocal
+// const arweave = Arweave.init({
+//   host: "localhost",
+//   port: 1984,
+//   protocol: "http",
+// });
+
+// const ardb = new ArDB(arweave);
+
+export default {
+  name: "BeatList",
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+</style>
